60941656d3762aa93c71e790a3c27eec5544105a
azu <azuciao@gmail.com>
2022-12-17 20:06:52 +0900

    chore: remove

--- pie.js
+++ pie.js
@@ -6,23 +6,6 @@ const pType = url.searchParams.get('type') ?? "pie";
 const commaToArray = (str) => {
     return str.split(',');
 };
-const createSortBy = ({ x, y, group }) => {
-    const sortedView = x.map((_, index) => {
-        return {
-            x: x[index],
-            y: y[index],
-            group: group[index]
-        }
-    });
-    if (sortBy === "x" || sortBy === "y" || sortBy === "group") {
-        return sortedView.sort((a, b) => {
-            return a[sortBy].localeCompare(b[sortBy], "ja", {
-                numeric: true
-            });
-        });
-    }
-    return sortedView;
-}
 const render = () => {
     const values = commaToArray(pValues);
     const labels = commaToArray(pLabels);
