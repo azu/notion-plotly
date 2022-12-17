f25de177af530385d464acbff7c512f357742a55
azu <azuciao@gmail.com>
2022-12-17 19:38:19 +0900

    add pie

---
+++ pie.js
@@ -0,0 +1,36 @@
+const main = document.getElementById('main');
+const url = new URL(location.href);
+const pValues = url.searchParams.get('values');
+const pLabels = url.searchParams.get('labels');
+const pType = url.searchParams.get('type') ?? "pie";
+const commaToArray = (str) => {
+    return str.split(',');
+};
+const createSortBy = ({ x, y, group }) => {
+    const sortedView = x.map((_, index) => {
+        return {
+            x: x[index],
+            y: y[index],
+            group: group[index]
+        }
+    });
+    if (sortBy === "x" || sortBy === "y" || sortBy === "group") {
+        return sortedView.sort((a, b) => {
+            return a[sortBy].localeCompare(b[sortBy], "ja", {
+                numeric: true
+            });
+        });
+    }
+    return sortedView;
+}
+const render = () => {
+    const values = commaToArray(pValues);
+    const labels = commaToArray(pLabels);
+    Plotly.newPlot(main, [{
+        type: pType,
+        values,
+        labels
+    }]);
+}
+render();
+
